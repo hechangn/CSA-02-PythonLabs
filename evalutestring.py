564fa283402d9e966fff846622e3ba608a64db07
hechangn <156150646+hechangn@users.noreply.github.com>
2024-01-30 23:39:48 +0700

    MESSAGE

--- evalutestring.py
+++ evalutestring.py
@@ -1,13 +1,14 @@
 #make a letter variable in range of alphabet
-letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #ask user to input the letters range
-range_of_letters = input("enter a range of letters (ex: a-z or a-g): \n")
+user_range = input("enter a range of letters (ex: a-z or a-g): \n")
 #split the letters range by hyphen "-"
-split = range_of_letters.split("-")
+split = user_range.split("-")
 #make the variable name start and end by their position in range_of_letters
-start = letters.index(split[0])
-end = letters.index(split[1])
+start = alphabet.index(split[0])
+end = alphabet.index(split[1])
 #use [] to make the range of letters by the user's order
-result_letters_range = letters[start:end + 1]
+result_string = alphabet[start:end + 1]
 #print the result
-result_display = print(result_letters_range)
+result_display = print(result_string)
+
